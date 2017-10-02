ca6254c45fd6f293496a4fb49569dbe1c1d289a5
Jordan Nordh <jdnordh@gmail.com>
2017-10-02 15:16:00 -0600

    Lots of changes

--- src/client/UrlCache.java
+++ src/client/UrlCache.java
@@ -2,19 +2,27 @@ package client;
 
 import java.io.BufferedReader;
 import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.io.StringBufferInputStream;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.util.Date;
 import java.util.ArrayList;
 
 public class UrlCache {
 
 	ArrayList<WebObject> catalog;
-	
+	final File folder = new File("");
+	File [] fileCatalog;
     /**
      * Default constructor to initialize data structures used for caching/etc
 	 * If the cache already exists then load it. If any errors then throw runtime exception.
@@ -22,7 +30,13 @@ public class UrlCache {
      * @throws IOException if encounters any errors/exceptions
      */
 	public UrlCache() throws IOException {
+		//TODO initilize a file array catalog
 		catalog = new ArrayList<WebObject>();
+		fileCatalog = folder.listFiles();
+		System.out.println("Files already in the system:");
+		for (int i = 0; i < fileCatalog.length; i++){
+			System.out.println(fileCatalog[i].getName());
+		}
 	}
 	
     /**
@@ -32,15 +46,6 @@ public class UrlCache {
      * @throws IOException if encounters any errors/exceptions
      */
 	public void getObject(String url) throws IOException {
-		
-		/*
-		 * String[] url = {"people.ucalgary.ca/~mghaderi/index.html",
-						"people.ucalgary.ca/~mghaderi/test/uc.gif",
-						"people.ucalgary.ca/~mghaderi/test/a.pdf",
-						"people.ucalgary.ca:80/~mghaderi/test/test.html"};
-		 * 
-		 */
-		//TODO url stuff
 		
 		String server = "";
 		String object = "";
@@ -65,11 +70,19 @@ public class UrlCache {
 			String portNumber = url.substring(colon + 1, firstslash);
 			port = Integer.parseInt(portNumber);
 		}
+		// check if the url is already cached
 		boolean haveit = false;
 		for (int i = 0; i < catalog.size(); i++){
 			if (catalog.get(i).getUrl().equals(url)){
+				// check if the file is too old (1 day in this case)
+				Date date = new Date();
+				long timeNow = date.getTime();
+				if ( (timeNow - catalog.get(i).lastModified()) > 86400) {
+					// file is too old, get a new version
+					break;
+				}
 				haveit = true;
-				System.out.println("Don't need to get " + url);
+				System.out.println("Don't need to get new version of: " + url);
 				break;
 			}
 		}
@@ -77,13 +90,13 @@ public class UrlCache {
 			//TODO get it
 			System.out.println("Need to get " + url);
 			try {
-				BufferedReader in;
+				InputStream in;
 				PrintWriter out;
 				System.out.println("Trying to connect to: " + server);
 				System.out.println("Trying to download: " + object);
 				
 				Socket socket = new Socket(InetAddress.getByName(server), port);
-				in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				in = socket.getInputStream();
 				out = new PrintWriter(socket.getOutputStream());
 				
 				// send the request
@@ -92,25 +105,63 @@ public class UrlCache {
 				out.println("\r");
 				out.flush();
 				
+				/*
 				System.out.println("The request sent was:");
 				System.out.println("GET " + object + " HTTP/1.1");
 				System.out.println("Host: " + server);
+				*/
 				
+				/*
 				StringBuffer file = new StringBuffer();
 				String buffer;
+				*/
 				
-				while ((buffer = in.readLine()) != null){
-					file.append(buffer);
-					file.append('\n');
+				
+				
+				String filename = server.replace('.', '_') + object.replace('/', '_');
+			
+				
+				
+				byte [] byteArray = new byte[1024 * 64];
+				int index = 0;
+				int headerEnd = 0;
+				while ( (byteArray[index++] = (byte)in.read()) != -1){
+					if (byteArray[index - 1] == '\r'){
+						headerEnd = 1;
+					}
+					if (headerEnd == 1 && byteArray[index - 1] == '\n'){
+						headerEnd = 2;
+						// Header is done, time to start reading into a file
+						// TODO ^^^
+					}
 				}
-				System.out.println("The file downloaded was: \n" + file.toString() + "\n\n");
-				// create object and add it to the catalog
-				WebObject download = new WebObject(url, file.toString());
-				catalog.add(download);
+				
+//				int index = 0;
+//				while ((byteArray[index++] = (byte) in.read(byteArray, 0, byteArray.length)) != -1){
+//					if (index > byteArray.length -1) throw new Exception("File is too big");
+//				}
+//				
+//				
+//				System.out.println("The file downloaded was: \n" + byteArray.toString() + "\n\n");
+//				// create object and add it to the catalog
+//				WebObject download = new WebObject(url, byteArray.toString());
+//				catalog.add(download);
+//				
+//				// TODO parse the header
+//				// TODO write file from byte array
+//				String filename = server.replace('.', '_') + object.replace('/', '_');
+//				
+//				FileOutputStream fos = new FileOutputStream("files\\" + filename);
+//				try {
+//					fos.write(byteArray);
+//				} finally {
+//					fos.close();
+//				}
+				
 				
 				socket.close();
 			} catch (Exception e) { 
-				System.err.println("Failed to connect to: " + url);
+				//System.err.println("Failed to connect to: " + url);
 				System.err.println("Error: "+ e.getMessage());
 			}
 		}
